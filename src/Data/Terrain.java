e860d0da91c96d0f0304c4939c8127ed0057e760
NBSamael <berthier.benjamin@gmail.com>
2013-11-07 12:49:32 +0100

    Modification de l'affichage texte du terrain de jeu

--- src/Data/Terrain.java
+++ src/Data/Terrain.java
@@ -1,11 +1,12 @@
 package Data;
+
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 
 public class Terrain extends HashMap<XY, Case> {
 	public Set<Personnage> personnages;
-	//private Map<Coordonnees, Case> cases;
+	// private Map<Coordonnees, Case> cases;
 	private int tailleX;
 	private int tailleY;
 
@@ -23,7 +24,7 @@ public class Terrain extends HashMap<XY, Case> {
 				if (j == 3 && i > 2 && i < 6)
 					put(new XY(j, i), new Case(new XY(j, i), 1));
 				else
-				// fin code pour un mur
+					// fin code pour un mur
 					put(new XY(j, i), new Case(new XY(j, i), 0));
 			}
 		}
@@ -36,7 +37,7 @@ public class Terrain extends HashMap<XY, Case> {
 	public int getTailleY() {
 		return tailleY;
 	}
-	
+
 	public Case get(int x, int y) {
 		return get(new XY(x, y));
 	}
@@ -48,7 +49,7 @@ public class Terrain extends HashMap<XY, Case> {
 	public boolean isCaseOccuped(int x, int y) {
 		return get(x, y).isOccuped();
 	}
-	
+
 	public boolean isCaseAvailable(XY position) {
 		return get(position).isFranchissable();
 	}
@@ -56,28 +57,29 @@ public class Terrain extends HashMap<XY, Case> {
 	public boolean isCaseOccuped(XY position) {
 		return get(position).isOccuped();
 	}
-	
+
 	public GrilleDeplacements calculeGrilleDeplacements(XY origine) {
 		return calculeGrilleDeplacements(origine, Integer.MAX_VALUE);
 	}
-	
+
 	public GrilleLigneDeVue calculeGrilleLigneDeVue(XY origine, int limite) {
 		GrilleLigneDeVue td = new GrilleLigneDeVue(this, origine);
 		td.calculeCiblesPossibles(limite);
 		return td;
 	}
-	
+
 	public GrilleLigneDeVue calculeGrilleLigneDeVue(XY origine) {
 		return calculeGrilleLigneDeVue(origine, Math.max(tailleX, tailleY));
 	}
-	
+
 	public GrilleDeplacements calculeGrilleDeplacements(XY origine, int limite) {
 		GrilleDeplacements td = new GrilleDeplacements(this, origine);
 		td.calculeDeplacementsPossibles(limite);
 		return td;
 	}
-	
-	public void deplacePersonnage (Personnage p, XY anciennePosition, XY nouvellePosition) {
+
+	public void deplacePersonnage(Personnage p, XY anciennePosition,
+			XY nouvellePosition) {
 		get(anciennePosition).setOccupant(null);
 		get(nouvellePosition).setOccupant(p);
 	}
@@ -85,27 +87,68 @@ public class Terrain extends HashMap<XY, Case> {
 	@Override
 	public String toString() {
 		String aff = new String();
-		for (int j = 1; j <= tailleX+2; j++) {
-			aff = aff.concat("X");
+		aff = aff.concat("    ");
+		for (int j = 1; j <= tailleX; j++) {
+			if (j < 10)
+				aff = aff.concat("  ");
+			else {
+				String index = new Integer(j).toString();
+				aff = aff
+						.concat(new Character(index.charAt(index.length() - 2))
+								.toString() + " ");
+			}
 		}
-		aff = aff.concat("\n");		
+		aff = aff.concat("\n    ");
+		for (int j = 1; j <= tailleX; j++) {
+			String index = new Integer(j).toString();
+			aff = aff.concat(new Character(index.charAt(index.length() - 1))
+					.toString() + " ");
+		}
+		aff = aff.concat("\n   ");
+		for (int j = 1; j <= tailleX; j++) {
+			aff = aff.concat("XX");
+		}
+		aff = aff.concat("X\n");
+
 		for (int i = 1; i <= tailleY; i++) {
-			aff = aff.concat("X");
+			if (i < 10)
+				aff = aff.concat(" " + i);
+			else {
+				aff = aff.concat(new Integer(i).toString());
+			}
+			aff = aff.concat(" X");
 			for (int j = 1; j <= tailleX; j++) {
 				Case c = get(new XY(j, i));
 				if (c.getOccupant() != null)
-					aff = aff.concat(c.getOccupant().nom.substring(0,1));
-				else if (c.getType()==0) 
+					aff = aff.concat(c.getOccupant().nom.substring(0, 1));
+				else if (c.getType() == 0)
 					aff = aff.concat(" ");
-				else if (c.getType()==1)
+				else if (c.getType() == 1)
 					aff = aff.concat("*");
-					//aff = aff.concat(String.valueOf((char) 219));
+				// aff = aff.concat(String.valueOf((char) 219));
+				if (j + 1 <= tailleX)
+					aff = aff.concat("|");
 			}
-		aff = aff.concat("X\n");
+			if (i + 1 <= tailleY)
+				aff = aff.concat("X\n   X");
+			else
+				aff = aff.concat("X\n   ");
+			if (i + 1 <= tailleY) {
+				for (int j = 1; j <= tailleX; j++) {
+					if (j + 1 <= tailleX)
+						aff = aff.concat("--");
+					else
+						aff = aff.concat("-");
+				}
+			}
+			if (i + 1 <= tailleY)
+				aff = aff.concat("X\n");
 		}
-		for (int j = 1; j <= tailleX+2; j++) {
-			aff = aff.concat("X");
+
+		for (int j = 1; j <= tailleX; j++) {
+			aff = aff.concat("XX");
 		}
+		aff = aff.concat("X");
 		return aff;
 	}
 }
