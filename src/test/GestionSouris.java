f164b843cbc0283615a97a73dd4e3e835c9d208c
NBSamael <berthier.benjamin@gmail.com>
2014-08-25 14:12:27 +0200

    Francisation de l'IHM

--- src/test/GestionSouris.java
+++ src/test/GestionSouris.java
@@ -1,65 +1,33 @@
 package test;
 
-import java.awt.Graphics2D;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseMotionListener;
-import java.awt.event.MouseWheelEvent;
-import java.awt.image.BufferedImage;
-
-import javax.swing.JScrollPane;
 
 public class GestionSouris extends MouseAdapter {
-	private JScrollPane comp;
 	private Picture picture;
 	boolean enfonce;
-	int vValue;
-	int hValue;
-	int oldX;
-	int oldY;
-	int caseX;
-	int caseY;
 
-	public GestionSouris(JScrollPane comp, Picture picture) {
+	private int clickX;
+	private int clickY;
+
+	public GestionSouris(Picture picture) {
 		super();
-		this.comp = comp;
 		this.picture = picture;
 		this.enfonce = false;
 	}
 
 	@Override
 	public void mouseClicked(MouseEvent e) {
-		System.out.println("X : " + caseX + " / Y : " + caseY);
+		System.out.println("Clicked (" + (e.getX() / 50) + ", "
+				+ (e.getY() / 50) + ")");
 	}
 
 	@Override
 	public void mousePressed(MouseEvent e) {
 		if (e.getButton() == MouseEvent.BUTTON3) {
 			enfonce = true;
-			oldX = e.getX();
-			oldY = e.getY();
-			vValue = comp.getVerticalScrollBar().getValue();
-			hValue = comp.getHorizontalScrollBar().getValue();
-		}
-	}
-
-	@Override
-	public void mouseMoved(MouseEvent e) {
-		if (caseX != (e.getX() / 50) || caseY != (e.getY() / 50)) {
-			caseX = e.getX() / 50;
-			caseY = e.getY() / 50;
-			reDessine(picture);
-			comp.setViewportView(picture);
-		}
-	}
-
-	@Override
-	public void mouseDragged(MouseEvent e) {
-		if (enfonce) {
-			comp.getVerticalScrollBar().setValue(new Double(vValue + ((oldY - e.getY()))*0.6).intValue());
-			comp.getHorizontalScrollBar()
-					.setValue(new Double(hValue + ((oldX - e.getX()))*0.5).intValue());
-	
+			clickX = e.getX();
+			clickY = e.getY();
 		}
 	}
 
@@ -68,33 +36,19 @@ public class GestionSouris extends MouseAdapter {
 		enfonce = false;
 	}
 
-	public void reDessine(Picture picture) {
-		BufferedImage imageCase = SpriteStore.get().getSprite("sprites/herbe50.jpg");
-		BufferedImage targetImage = SpriteStore.get().getSprite("sprites/herbe50.jpg_rg_lg");
+	@Override
+	public void mouseMoved(MouseEvent e) {
 
-		int nbX = 100;
-		int nbY = 60;
-
-		BufferedImage imagePlateau = new BufferedImage(imageCase.getWidth()
-				* nbX, imageCase.getHeight() * nbY, imageCase.getType());
-		Graphics2D graphiqueImagePlateau = imagePlateau.createGraphics();
-
-		for (int x = 0; x < nbX; x++) {
-			for (int y = 0; y < nbY; y++) {
-				if (x == caseX && y == caseY) {
-
-					graphiqueImagePlateau.drawImage(targetImage, null, x
-							* imageCase.getWidth(), y * imageCase.getHeight());
-
-				} else {
-					
-					graphiqueImagePlateau.drawImage(imageCase, null, x
-							* imageCase.getWidth(), y * imageCase.getHeight());
-				}
-			}
-		}
-
-		picture.setImage(imagePlateau);
 	}
 
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		if (enfonce) {
+			int dx = clickX - e.getX();
+			int dy = clickY - e.getY();
+			picture.moveView(dx, dy);
+			clickX = e.getX();
+			clickY = e.getY();
+		}
+	}
 }
--- src/test/Picture.java
+++ src/test/Picture.java
@@ -1,51 +1,87 @@
 package test;
+
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Image;
-import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
 
 import javax.swing.JPanel;
-import javax.swing.Scrollable;
-
 
 @SuppressWarnings("serial")
-class Picture extends JPanel implements Scrollable {
-    private Image image;
-    private int width, height;
-    @Override
-    public Dimension getPreferredSize() {
-        Dimension size = super.getPreferredSize();
-        size.width = width;
-        size.height = height;
-        return size;
-    }
-    @Override
-    protected void paintComponent(final Graphics g) {
-        super.paintComponent(g);
-        g.drawImage(image, 0, 0, this);
-    }
-    public void setImage(final Image img) {
-        this.image = img;
-        width = img.getWidth(this);
-        height = img.getHeight(this);
-        revalidate();
-        repaint();
-    }
-    public Dimension getPreferredScrollableViewportSize() {
-        return getPreferredSize();
-    }
-    public int getScrollableUnitIncrement(Rectangle visibleRect,
-            int orientation, int direction) {
-        return 100;
-    }
-    public int getScrollableBlockIncrement(Rectangle visibleRect,
-            int orientation, int direction) {
-        return 200;
-    }
-    public boolean getScrollableTracksViewportWidth() {
-        return false;
-    }
-    public boolean getScrollableTracksViewportHeight() {
-        return false;
-    }
+class Picture extends JPanel {
+	private Image image;
+	private int width, height;
+
+	private BufferedImage imageCase;
+
+	private int nbX = 30;
+	private int nbY = 20;
+
+	private int viewX = 0;
+	private int viewY = 0;
+
+	public Picture() {
+		imageCase = SpriteStore.get().getSprite("sprites/herbe50.jpg");
+
+		BufferedImage imagePlateau = new BufferedImage(imageCase.getWidth()
+				* nbX, imageCase.getHeight() * nbY, imageCase.getType());
+		Graphics2D g = imagePlateau.createGraphics();
+
+		for (int x = 0; x < nbX; x++) {
+			for (int y = 0; y < nbY; y++) {
+				g.drawImage(imageCase, null, x * imageCase.getWidth(), y
+						* imageCase.getHeight());
+
+				g.drawString(x + "/" + y, x * imageCase.getWidth(), y
+						* imageCase.getHeight() + imageCase.getHeight());
+			}
+
+		}
+
+		setImage(imagePlateau);
+
+		GestionSouris gs = new GestionSouris(this);
+		addMouseListener(gs);
+		addMouseMotionListener(gs);
+
+	}
+
+	public void moveView(int dx, int dy) {
+		viewX += dx;
+		if (viewX < 0)
+			viewX = 0;
+		if (viewX > nbX * imageCase.getWidth() - getWidth())
+			viewX = nbX * imageCase.getWidth() - getWidth();
+		viewY += dy;
+		if (viewY < 0)
+			viewY = 0;
+		if (viewY > nbY * imageCase.getHeight() - getHeight())
+			viewY = nbY * imageCase.getHeight() - getHeight();
+		repaint();
+	}
+
+	@Override
+	public Dimension getPreferredSize() {
+		Dimension size = super.getPreferredSize();
+		size.width = width;
+		size.height = height;
+		return size;
+	}
+
+	@Override
+	protected void paintComponent(final Graphics g) {
+		super.paintComponent(g);
+
+		g.drawImage(image, 0, 0, getWidth(), getHeight(), viewX, viewY, viewX
+				+ getWidth(), viewY + getHeight(), null);
+	}
+
+	public void setImage(final Image img) {
+		this.image = img;
+		width = img.getWidth(this);
+		height = img.getHeight(this);
+		revalidate();
+		repaint();
+	}
 }
